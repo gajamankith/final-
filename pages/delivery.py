5a305361d90677b8353c2abef917de7833d6df39
gajamankith <asustuf5370@gmail.com>
2026-06-14 12:23:29 +0000

    hi

---
+++ pages/delivery.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from utils.db import conn, cursor
+
+st.title("🚚 Courier Agent Portal")
+
+student_name = st.text_input("Student Name")
+tracking_id = st.text_input("Tracking ID")
+company = st.text_input("Courier Company")
+
+if st.button("Register Package"):
+
+    cursor.execute("""
+    INSERT INTO packages
+    (student_name, tracking_id, courier_company, status)
+    VALUES(?,?,?,?)
+    """,
+    (
+        student_name,
+        tracking_id,
+        company,
+        "Received at Campus Center"
+    ))
+
+    conn.commit()
+
+    st.success("Package Registered Successfully")
