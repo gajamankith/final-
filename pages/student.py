743329f2657030c10045c7439e1dd5941367b19e
gajamankith <asustuf5370@gmail.com>
2026-06-14 13:05:44 +0000

    hi

--- pages/student.py
+++ pages/student.py
@@ -20,24 +20,4 @@ if st.button("Search Package"):
         st.dataframe(df)
     else:
         st.warning("No Packages Found")
-        import random
-
-otp = random.randint(100000,999999)
-st.session_state["otp"] = otp
-user_otp = st.text_input("Enter OTP")
-
-if st.button("Verify"):
-
-    if user_otp == str(st.session_state["otp"]):
-        st.success("Verified")
-
-    else:
-        st.error("Wrong OTP")
-        st.image
-    (package_qr_path,
-    width=250)
-cursor.execute("""
-UPDATE packages
-SET status='Collected'
-WHERE tracking_id=?
-""",(tracking_id,))
+       
