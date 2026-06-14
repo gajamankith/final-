410fc702ee31423584e7c7bb8d8b6182c3182b88
gajamankith <asustuf5370@gmail.com>
2026-06-14 12:43:16 +0000

    hi# Please enter the commit message for your changes. Lines starting

--- pages/delivery.py
+++ pages/delivery.py
@@ -74,3 +74,15 @@ def send_email(email, tracking_id):
 
     server.quit()
     send_email(student_email, tracking_id)
+    from twilio.rest import Client
+
+client = Client(
+    ACCOUNT_SID,
+    AUTH_TOKEN
+)
+
+client.messages.create(
+    body=f"Package Arrived. Tracking ID: {tracking_id}",
+    from_='whatsapp:+14155238886',
+    to=f'whatsapp:{student_phone}'
+)
